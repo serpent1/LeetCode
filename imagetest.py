8e7acf59da2faa0859a9b1c7f7b75027fca3886c
songsong1120 <69299320+songsong1120@users.noreply.github.com>
2021-11-01 21:29:25 +0800

    add CONTOUR

--- imagetest.py
+++ imagetest.py
@@ -1,14 +1,13 @@
-from PIL import Image
+from PIL import Image,ImageFilter
 
 image = Image.open('image.jpg')
 print(image.format,image.size,image.mode)
 image.show()
 
-rect = (80, 20, 310, 360)
-image.crop(rect).show()
-size=(128,128)
-test=image.thumbnail(size)
-image.show()
-print(image.size)
-
-
+# rect = (80, 20, 310, 360)
+# image.crop(rect).show()
+# size=(128,128)
+# test=image.thumbnail(size)
+# image.show()
+# print(image.size)
+image.filter(ImageFilter.CONTOUR).show()
